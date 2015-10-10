0b7d56ae9d2457b78292447c826ed112a4acd487
Tom Smits <d167647@edu.rocwb.nl>
2015-10-11 00:23:26 +0200

    Added DatabaseHandler

---
+++ Barroc IT - Code/BarrocIT/BarrocIT/DatabaseHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public class DatabaseHandler
+{
+    SqlConnection SQLConn;
+	public DatabaseHandler(string SQLConnString)
+	{
+        SQLConn = new SqlConnection(SQLConnString);
+	}
+
+    public void openConnection()
+    {
+        SQLConn.Open();
+    }
+
+    public void closeConnection()
+    {
+        SQLConn.Close();
+    }
+
+    public void disposeConnection()
+    {
+        SQLConn.Dispose();
+    }
+
+    public void testConnection()
+    {
+        try
+        {
+            openConnection();
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show("There occurred a error!" + exception);
+        }
+        finally
+        {
+            closeConnection();
+        }
+    }
+}
