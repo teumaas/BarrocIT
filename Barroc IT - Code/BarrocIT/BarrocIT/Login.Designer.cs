e7b2adb90633124ea5e793dbeff8654945cf7834
Tom Smits <d167647@edu.rocwb.nl>
2015-10-11 12:27:51 +0200

    Edit

--- Barroc IT - Code/BarrocIT/BarrocIT/Login.Designer.cs
+++ Barroc IT - Code/BarrocIT/BarrocIT/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BarrocIT
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(308, 234);
+            this.Name = "Login";
+            this.Text = "Barroc IT - Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
